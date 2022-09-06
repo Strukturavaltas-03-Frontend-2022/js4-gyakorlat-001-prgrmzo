e6dfa7898dfe5b16cd2f5da0c10efc2e2e164ae6
Zo <pahyzoard@outlook.hu>
2022-09-06 18:00:16 +0200

    mission completed

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,15 @@
+const convertToUppercase = (arr) => {
+  return new Promise((resolve, reject) => {
+    let uppercaseArr = [];
+    arr.forEach((item) => {
+      if (typeof item === "string") {
+        uppercaseArr.push(item.toLocaleUpperCase());
+      } else {
+        reject(new Error("Error: Not all items in the array are strings!"));
+      }
+    });
+    resolve(uppercaseArr);
+  });
+};
+
 export default convertToUppercase;
--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,15 @@
+const sortStrings = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(arr)) {
+      reject(new Error("Error: Something went wrong with sorting words!"));
+    }
+    arr.forEach((item) => {
+      if (typeof item !== "string") {
+        reject(new Error("Error: Not all items in the array are strings!"));
+      }
+    });
+    resolve(arr.sort());
+  });
+};
+
 export default sortStrings;
